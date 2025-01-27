6b7e155df4670e9076eb1a89bb43b1a1d5d23920
Esther <esther.mothanaprakoon@gmail.com>
2025-01-27 21:03:51 +0700

    add 1

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // ชื่อไฟล์ฐานข้อมูล JSON
+const router = jsonServer.router({
+    "posts": [
+      {
+        "author": "Ron Jeffrey\n",
+        "quote": "Code never lies, comments did",
+        "id": "3"
+      },
+      {
+        "author": "Jeff Atwood",
+        "quote": "The best code is no code at all.",
+        "id": "4"
+      },
+      {
+        "author": "Casey Patton",
+        "quote": "Programming is thinking, not typing",
+        "id": "6"
+      },
+      {
+        "author": "Mike",
+        "quote": "If the code doesn't bother you, don't bother it",
+        "id": "fEKKQy3"
+      },
+      {
+        "author": "",
+        "quote": "Computers are fast, programmers keep it slow  ",
+        "id": "oUuWlwb"
+      }
+    ]
+  });
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
